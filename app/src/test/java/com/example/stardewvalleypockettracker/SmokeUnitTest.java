5d899ad4a0676c08890b059b88fa6c06712b760e
Crystal Quick <cacquick@gmail.com>
2019-07-14 14:47:30 -0500

    Add travisCI config.

---
+++ app/src/test/java/com/example/stardewvalleypockettracker/SmokeUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.stardewvalleypockettracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SmokeUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
