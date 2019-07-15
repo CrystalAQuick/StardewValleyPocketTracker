fc2de8dbefad53086de6ba41d991378e084b27cf
Crystal Quick <cacquick@gmail.com>
2019-07-14 21:30:52 -0500

    Add custom toolbar and settings icon.

--- app/src/main/java/com/example/stardewvalleypockettracker/MainActivity.java
+++ app/src/main/java/com/example/stardewvalleypockettracker/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.stardewvalleypockettracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_icon, menu);
+        return true;
     }
 }
